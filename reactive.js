c4c2d75a5d60c57b4d2579256b99993242c2fdaf
timokie <timokie@126.com>
2020-11-04 00:43:10 +0800

    feat: 完善数据响应式方法（解决对象嵌套&赋值为对象时数据变为非响应式问题）

--- reactive.js
+++ reactive.js
@@ -1,4 +1,12 @@
+/**
+ * 将对象转化为响应式数据
+ * @param {*} obj 需要响应化的对象
+ * @param {*} key 属性
+ * @param {*} val 值
+ */
 function defineReactive (obj, key, val) {
+  // 解决诸如 test.baz.a 对象嵌套问题
+  observe(val)
   Object.defineProperty(obj, key, {
     get() {
       console.log(`get ${key}: ${val}`)
@@ -8,11 +16,18 @@ function defineReactive (obj, key, val) {
       if (newVal!==val) {
         console.log(`set ${key}: ${newVal}`)
         val = newVal
+        // 解决赋的值是对象的情况(譬如test.foo={f1: 666})
+        observe(val)
       }
     }
   })
 }
-// 遍历对象所有属性
+
+
+/**
+ * 对象响应化:遍历每个key，定义getter、setter
+ * @param {*} data 需要响应化的对象
+ */
 function observe (data) {
   if(typeof data !== 'object' || data === null) {
     return
@@ -24,12 +39,20 @@ function observe (data) {
 }
 const test = {
   foo: 'foo',
-  bar: 'bar'
+  bar: 'bar',
+  baz: {
+    a: 4
+  }
 }
 
 // defineReactive(test, 'foo', 'fooval')
 observe(test)
-test.foo
-test.bar
-test.foo = 'fooooooooo'
-test.bar = 'barrrrr'
+// test.foo
+// test.bar
+// test.foo = 'fooooooooo'
+// test.bar = 'barrrrr'
+// test.baz.a
+test.foo={
+  f1: '666'
+}
+test.foo.f1
