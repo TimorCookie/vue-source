4641ce46ea6de9a8ea8fc12b3488acc90af4fea0
timokie <timokie@126.com>
2020-11-16 15:22:20 +0800

    feat: 真实dom直接渲染版本

--- tvue2/tvue2.js
+++ tvue2/tvue2.js
@@ -56,130 +56,151 @@ class Observer {
 }
 class TVue {
   constructor(options) {
-    this.options = options
+    this.$options = options
     this.$data = options.data
     // ! 1.数据响应式
     observe(this.$data)
     // ! 1.5 代理 将data中的所有属性代理到JVue实例上方便用户使用
     proxy(this)
     // ! 2.编译
-    new Compile(options.el, this)
+    // new Compile(options.el, this)
+    if (options.el) {
+      this.$mount(options.el)
+    }
+  }
+  $mount (el) {
+    // 获取宿主元素
+    this.$el = document.querySelector(el)
+    console.log(this.$el)
+    const updateComponent = () => {
+       // 执行渲染函数
+      const { render } = this.$options;
+      // 真实dom操作版实现
+      const el = render.call(this);
+      const parent = this.$el.parentElement;
+      parent.insertBefore(el, this.$el.nextSibling);
+      parent.removeChild(this.$el);
+      this.$el = el;
+    }
+    // 创建一个 Watcher 实例
+    new Watcher(this, updateComponent)
   }
 }
 // 将宿主的模板编译，获取它里面的动态内容，找到相关依赖并且生成watcher
-class Compile {
-  constructor(el, vm) {
-    this.$el = document.querySelector(el)
-    this.$vm = vm
+// class Compile {
+//   constructor(el, vm) {
+//     this.$el = document.querySelector(el)
+//     this.$vm = vm
 
-    this.compile(this.$el)
-  }
+//     this.compile(this.$el)
+//   }
 
-  compile(el) {
-    const childNodes = el.childNodes
-    childNodes.forEach(node => {
-      if (this.isElement(node)) {
-        // element
-        this.compileElement(node)
-      } else if (this.isInerpolation(node)) {
-        // 编译插值 text
-        this.compileText(node)
-      }
-      // 递归遍历子节点
-      if (node.childNodes) {
-        this.compile(node)
-      }
-    })
-  }
-  isElement(node) {
-    return node.nodeType === 1
-  }
-  isInerpolation(node) {
-    return node.nodeType === 3 && (/\{\{(.*)\}\}/).test(node.textContent)
-  }
-  compileText(node) {
-    this.update(node, RegExp.$1, 'text')
-    // node.textContent = this.$vm[RegExp.$1]
-  }
-  compileElement(node) {
-    let nodeAttrs = node.attributes
-    // 遍历元素所有特性 如果为k-开头 说明是动态，需要特殊处理
-    Array.from(nodeAttrs).forEach(attr => {
-      // 形如 j-text="counter"
-      let attrName = attr.name
-      let exp = attr.value
-      if (this.isDirective(attrName)) {
-        // ! dir: 指令
-        const dir = attrName.substring(2)
-        // 执行指令对应的方法
-        this[dir] && this[dir](node, exp)
-      }
-    })
-  }
-  isDirective(attr) {
-    return attr.startsWith('t-')
-  }
-  text(node, exp) {
-    this.update(node, exp, 'text')
-    // node.textContent = this.$vm[exp]
-  }
-  html(node, exp) {
-    this.update(node, exp, 'html')
-    // node.innerHTML = this.$vm[exp]
-  }
+//   compile(el) {
+//     const childNodes = el.childNodes
+//     childNodes.forEach(node => {
+//       if (this.isElement(node)) {
+//         // element
+//         this.compileElement(node)
+//       } else if (this.isInerpolation(node)) {
+//         // 编译插值 text
+//         this.compileText(node)
+//       }
+//       // 递归遍历子节点
+//       if (node.childNodes) {
+//         this.compile(node)
+//       }
+//     })
+//   }
+//   isElement(node) {
+//     return node.nodeType === 1
+//   }
+//   isInerpolation(node) {
+//     return node.nodeType === 3 && (/\{\{(.*)\}\}/).test(node.textContent)
+//   }
+//   compileText(node) {
+//     this.update(node, RegExp.$1, 'text')
+//     // node.textContent = this.$vm[RegExp.$1]
+//   }
+//   compileElement(node) {
+//     let nodeAttrs = node.attributes
+//     // 遍历元素所有特性 如果为k-开头 说明是动态，需要特殊处理
+//     Array.from(nodeAttrs).forEach(attr => {
+//       // 形如 j-text="counter"
+//       let attrName = attr.name
+//       let exp = attr.value
+//       if (this.isDirective(attrName)) {
+//         // ! dir: 指令
+//         const dir = attrName.substring(2)
+//         // 执行指令对应的方法
+//         this[dir] && this[dir](node, exp)
+//       }
+//     })
+//   }
+//   isDirective(attr) {
+//     return attr.startsWith('t-')
+//   }
+//   text(node, exp) {
+//     this.update(node, exp, 'text')
+//     // node.textContent = this.$vm[exp]
+//   }
+//   html(node, exp) {
+//     this.update(node, exp, 'html')
+//     // node.innerHTML = this.$vm[exp]
+//   }
   
-  /**
-   * 初始化显示， watcher创建
-   * @param {*} node 节点
-   * @param {*} exp 属性值
-   * @param {*} dir 指令（k-`${dir}`）
-   */
-  update(node, exp, dir) {
-    // 1.初始化
-    const fn = this[dir + 'Updater'];
-    fn && fn(node, this.$vm[exp])
-    // 2.更新 创建watcher
-    new Watcher(this.$vm, exp, function(val) {
-      fn && fn(node, val)
-    })
-  }
-  // 节点更新实操方法
-  textUpdater (node, val){
-    node.textContent = val
-  }
-  htmlUpdater (node, val) {
-    node.innerHTML = val
-  }
-}
+//   /**
+//    * 初始化显示， watcher创建
+//    * @param {*} node 节点
+//    * @param {*} exp 属性值
+//    * @param {*} dir 指令（k-`${dir}`）
+//    */
+//   update(node, exp, dir) {
+//     // 1.初始化
+//     const fn = this[dir + 'Updater'];
+//     fn && fn(node, this.$vm[exp])
+//     // 2.更新 创建watcher
+//     new Watcher(this.$vm, exp, function(val) {
+//       fn && fn(node, val)
+//     })
+//   }
+//   // 节点更新实操方法
+//   textUpdater (node, val){
+//     node.textContent = val
+//   }
+//   htmlUpdater (node, val) {
+//     node.innerHTML = val
+//   }
+// }
 
 
 // 负责视图更新，与依赖一一对应
 class Watcher {
-  constructor(vm, key, update) {
-    console.log('key:', key)
+  constructor(vm, expOrFn) {
     this.vm = vm;
-    this.key = key;
-    this.updateFn = update;
-    // 触发依赖收集
-    Dep.target = this;
-    this.vm[this.key];
-    Dep.target = null
+    this.getter = expOrFn;
+     // 触发依赖收集
+    this.get()
 
+  }
+  get () {
+    Dep.target = this;
+    this.getter.call(this.vm)
+    Dep.target = null
   }
   // Dep未来会通知更新
   update() {
-    this.updateFn.call(this.vm, this.vm[this.key])
+    this.get()
   }
 }
 // 依赖：和响应式对象的key一一对应
 class Dep {
   constructor() {
-    this.deps = [];
+    this.deps = new Set();
   }
-  addDep(dep) {
-    this.deps.push(dep)
+  addDep(wather) {
+    this.deps.add(wather)
   }
   notify() {
-    this.deps.forEach(dep => dep.update())
+    this.deps.forEach(wather => wather.update())
   }
 }
